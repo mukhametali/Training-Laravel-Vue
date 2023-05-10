7f2041f31857263d276eabbf1210020c81119692
mukhametali <m.kussaiynov1996@gmail.com>
2023-05-10 11:11:59 +0600

    Create first component Vue

---
+++ resources/js/components/PostComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="">
+        111111111111111
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PostComponent"
+}
+</script>
+
+<style scoped>
+
+</style>
