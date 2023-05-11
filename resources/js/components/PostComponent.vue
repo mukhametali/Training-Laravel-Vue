54a16c5fca8cdcdf70a6d3c02c5d7015790383b7
mukhametali <m.kussaiynov1996@gmail.com>
2023-05-11 12:08:37 +0600

    asynchronous CRUD. create person with axios and v-model

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,24 +1,25 @@
 <template>
     <div>
+        <CreateComponent></CreateComponent>
         <SinglePostComponent></SinglePostComponent>
-            <table class="table">
-                <thead>
-                <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Age</th>
-                    <th scope="col">Job</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr v-for="person in persons">
-                    <th scope="row">{{ person.id }}</th>
-                    <td>{{ person.name }}</td>
-                    <td>{{ person.age }}</td>
-                    <td>{{ person.job }}</td>
-                </tr>
-                </tbody>
-            </table>
+<!--            <table class="table">-->
+<!--                <thead>-->
+<!--                <tr>-->
+<!--                    <th scope="col">ID</th>-->
+<!--                    <th scope="col">Name</th>-->
+<!--                    <th scope="col">Age</th>-->
+<!--                    <th scope="col">Job</th>-->
+<!--                </tr>-->
+<!--                </thead>-->
+<!--                <tbody>-->
+<!--                <tr v-for="person in persons">-->
+<!--                    <th scope="row">{{ person.id }}</th>-->
+<!--                    <td>{{ person.name }}</td>-->
+<!--                    <td>{{ person.age }}</td>-->
+<!--                    <td>{{ person.job }}</td>-->
+<!--                </tr>-->
+<!--                </tbody>-->
+<!--            </table>-->
         </div>
 </template>
 
@@ -26,6 +27,7 @@
 
 <script>
 import SinglePostComponent from "./SinglePostComponent";
+import CreateComponent from "./CreateComponent";
 export default {
     name: "PostComponent",
 
@@ -36,20 +38,15 @@ export default {
         }
     },
     mounted() {
-      this.getPersons()
     },
 
     methods: {
-        getPersons() {
-            axios.get('/persons')
-            .then(res => {
-                this.persons = res.data
-            })
-        }
+
     },
 
     components: {
-        SinglePostComponent
+        SinglePostComponent,
+        CreateComponent
     }
 }
 </script>
